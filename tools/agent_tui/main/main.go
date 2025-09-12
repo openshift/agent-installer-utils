0c274c4053942090ee6464d89ba5eeef414380d6
Zane Bitter <zbitter@redhat.com>
2025-09-13 00:31:22 +1200

    Check for presence of registry instead of WorkflowType
    We ultimately want to trigger behaviour based on what features are
    present (in this case, if there is a local registry then we don't need
    to check connectivity to the registry) rather than maintain a matrix of
    all the different modes and a distributed definition of what features
    are available in each.

--- tools/agent_tui/main/main.go
+++ tools/agent_tui/main/main.go
@@ -29,27 +29,24 @@ func main() {
 		logPath = "/tmp/agent_tui.log"
 		fmt.Printf("AGENT_TUI_LOG_PATH is unspecified, logging to: %v\n", logPath)
 	}
-	rendezvousIP, workflowType := getRendezvousHostEnv()
+	rendezvousIP := getRendezvousIP()
 	config := checks.Config{
 		ReleaseImageURL: releaseImage,
 		LogPath:         logPath,
-		WorkflowType:    workflowType,
 	}
 	agent_tui.App(nil, rendezvousIP, config)
 }
 
-// This function reads /etc/assisted/rendezvous-host.env
-// for NODE_ZERO_IP and WORKFLOY_TYPE.
-func getRendezvousHostEnv() (nodeZeroIP, workflowType string) {
+// getRendezvousIP reads NODE_ZERO_IP from /etc/assisted/rendezvous-host.env.
+func getRendezvousIP() (nodeZeroIP string) {
 	envMap, err := godotenv.Read(ui.RENDEZVOUS_HOST_ENV_PATH)
 	if err != nil {
-		return "", ""
+		return ""
 	}
 	nodeZeroIP = envMap["NODE_ZERO_IP"]
 	if nodeZeroIP == RENDEZVOUS_IP_TEMPLATE_VALUE {
 		nodeZeroIP = ""
 	}
-	workflowType = envMap["WORKFLOW_TYPE"]
 
-	return nodeZeroIP, workflowType
+	return nodeZeroIP
 }
